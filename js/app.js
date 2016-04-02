19ab4914104b0f4383094563953b31c4431bdbc9
Dwight Gunning <dwight@dwightgunning.com>
2016-04-02 14:10:44 +1000

    Completed first draft basic copy

--- js/app.js
+++ js/app.js
@@ -2,20 +2,43 @@ $(document).foundation();
 
 // Not the best code - needs some CSS refactoring me thinks
 function cardwidth() {
-	$(".flip-container").each(function() {
-		$(this).css('width', '');
-		$(this).css('height', '');
+  $(".flip-container").each(function() {
+    $(this).css("width", "");
+    $(this).css("height", "");
 
-		var $front = $(this).find("div.front");
-		var $frontWidth = $front.outerWidth();
-		var $frontHeight = $front.outerHeight();
+    var $front = $(this).find("div.front");
+    var $frontWidth = $front.outerWidth();
+    var $frontHeight = $front.outerHeight();
 
-		$(this).css('width', $frontWidth + 'px');
-		$(this).css('height', $frontHeight + 'px');
-	});
+    $(this).css("width", $frontWidth + "px");
+    $(this).css("height", $frontHeight + "px");
+  });
 }
 cardwidth();
 
 $(window).resize(function() {
-	cardwidth();
+  cardwidth();
+});
+
+$("#rolodex-form").submit(function(event) {
+  event.preventDefault();
+
+  var form_json = {
+    'name': $("#name-input").val(),
+    'email': $("#email-input").val(),
+    'twitter': $("#twitter-input").val(),
+  }
+
+  $.ajax({
+    url: "https://formspree.io/dwight@dwightgunning.com",
+    method: "POST",
+    data: form_json,
+    dataType: "json"
+  })
+    .done(function(data) {
+      $("#rolodex-submission-confirmed-modal").foundation("open");
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+      $("#rolodex-form-submit-error").show();
+    });
 });
