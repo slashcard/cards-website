725ba88ef9af8ee66fe0ed68d34bed0dd21ef018
Jordan Gardiner <hello@jordsta.com>
2016-05-02 11:05:03 +1000

    Stripped out card flip functionality

--- js/app.js
+++ js/app.js
@@ -1,25 +1,5 @@
 $(document).foundation();
 
-// Not the best code - needs some CSS refactoring me thinks
-function cardwidth() {
-  $(".flip-container").each(function() {
-    $(this).css("width", "");
-    $(this).css("height", "");
-
-    var $front = $(this).find("div.front");
-    var $frontWidth = $front.outerWidth();
-    var $frontHeight = $front.outerHeight();
-
-    $(this).css("width", $frontWidth + "px");
-    $(this).css("height", $frontHeight + "px");
-  });
-}
-cardwidth();
-
-$(window).resize(function() {
-  cardwidth();
-});
-
 $("#rolodex-form").submit(function(event) {
   event.preventDefault();
 
